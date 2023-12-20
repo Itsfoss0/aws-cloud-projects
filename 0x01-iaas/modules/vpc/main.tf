2e445c0155ccc5e92086fbd7d0fbba0d0cf761a8
Itsfoss0 <com01321@uoeld.ac.ke>
2023-12-20 16:35:05 +0300

    [Chore]: Revert modules

--- 0x01-iaas/modules/vpc/main.tf
+++
@@ -1,9 +0,0 @@
-# VPC module 
-
-resource "aws_vpc" "web_vpc" {
-  cidr_block           = var.cidr_block
-  enable_dns_hostnames = true
-  enable_dns_support   = true
-
-  tags = var.tags
-}
--- 0x01-iaas/modules/vpc/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "cidr_block" {
-    type = string
-    description = "CIDR block range for the VPC" 
-}
-
-variable "tags" {
-    description = "Tags for the VPC"
-    type = map(string)
-}
