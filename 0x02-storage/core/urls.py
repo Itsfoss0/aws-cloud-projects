873b2ca19622e8871fa2fb6e4be384ff2720f57c
Itsfoss0 <com01321@uoeld.ac.ke>
2023-12-22 20:00:08 +0300

    [Chore]: Add django project

--- 0x02-storage/core/urls.py
+++ 0x02-storage/core/urls.py
@@ -2,8 +2,9 @@
 Base project url configs
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('todo.urls')),
 ]
