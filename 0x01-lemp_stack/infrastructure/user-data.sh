d10904a4abce67ecc7dd228b863aec9053ec4b59
Itsfoss0 <com01321@uoeld.ac.ke>
2024-03-23 17:20:23 +0300

    [chore]: update user data script to install php-fpm

--- 0x01-lemp_stack/infrastructure/user-data.sh
+++ 0x01-lemp_stack/infrastructure/user-data.sh
@@ -6,7 +6,7 @@
 
 sudo hostnamectl set-hostname lemp-02
 sudo apt-get update --yes
-sudo apt-get install nginx php libapache2-mod-php php-mysql mysql-server --yes
+sudo apt-get install nginx php php-fpm libapache2-mod-php php-mysql mysql-server --yes
 
 sudo mkdir /var/www/projectlemp
 sudo chown -R "$USER:$USER" /var/www/projectlemp/
